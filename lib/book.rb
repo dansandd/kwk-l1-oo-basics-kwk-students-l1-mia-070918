ceb947bde89a098269b939b180e3dcd601682935
dansandd <dana.sandler@gmail.com>
2018-07-12 17:24:24 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,11 +4,18 @@ class Book
   def initialize(title)
     @title = title
   end
+  
   def title
     @title
   end
+  
   def author=(author)
     @author = author
   end
+  
+  def author 
+    @author
+  end
+  
   
 end
